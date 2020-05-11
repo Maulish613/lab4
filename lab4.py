be1b8dedc60f1c731c047dd76ce437faef37d386
Maulish Shah <ml344699@dal.ca>
2020-05-11 16:46:16 -0300

    Task 1 completed

--- lab4.py
+++ lab4.py
@@ -9,6 +9,17 @@ import string
 fin = open('words.txt')
 for i in fin:
     words = i.split()
-    print(words)
+    #print(words)
+    for word in words:
+        whitespace = string.whitespace
+        punctuation = string.whitespace
+        both = whitespace+punctuation
+        word = word.strip(both)
+        word_list = ''
+        for each_letter in word:
+            word_without_something = string.printable
+            if each_letter in word_without_something:
+                word_list+= each_letter
+        print(word_list)
 
     
