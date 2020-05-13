ccef988ab01e345e3e48e8e9afd3c831b584481c
Maulish Shah <ml344699@dal.ca>
2020-05-13 14:31:52 -0300

    task3 created and task 2 completed

--- task2.py
+++ task2.py
@@ -1,8 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 11 16:47:02 2020
+import string
+from collections import Counter
+fin = open('pg2542.txt')
+count = 0
+unique=[] 
+for i in fin:
+    words = i.split()
+    for word in words:
+        white_space = string.whitespace
+        punctuation = string.punctuation
+        word = word.strip(white_space + punctuation)
+        new_word = ''
+        for j in word:
+            printing_word = string.printable
+            if j in printing_word:
+                new_word+= j
+                count+=1
+        unique.append(new_word)
+        #print(new_word)
+ 
+#print(set(unique))
+#print(count)
 
-@author: mauli
-"""
+frequent_words = dict(Counter(unique))
 
+#print(frequent_words)    
+print(type(frequent_words))
 
+c = Counter(frequent_words)
+print(c.most_common(20))
+    
