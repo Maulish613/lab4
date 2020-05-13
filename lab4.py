1a91862779ddec617b7f5cfde0294744f6526fc5
Maulish Shah <ml344699@dal.ca>
2020-05-13 12:46:10 -0300

    lab4.py file done and book added

--- lab4.py
+++ lab4.py
@@ -6,20 +6,18 @@ Created on Mon May 11 15:30:21 2020
 """
 import string
 
-fin = open('words.txt')
+fin = open('pg2542.txt')
+
 for i in fin:
     words = i.split()
-    #print(words)
     for word in words:
-        whitespace = string.whitespace
-        punctuation = string.whitespace
-        both = whitespace+punctuation
-        word = word.strip(both)
-        word_list = ''
-        for each_letter in word:
-            word_without_something = string.printable
-            if each_letter in word_without_something:
-                word_list+= each_letter
-        print(word_list)
-
-    
+        white_space = string.whitespace
+        punctuation = string.punctuation
+        word = word.strip(white_space + punctuation)
+        new_word = ''
+        for j in word:
+            printing_word = string.printable
+            if j in printing_word:
+                new_word+= j
+        print(new_word)
+                
